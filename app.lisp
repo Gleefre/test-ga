1b239ffe080027fae25307551b1b29ae97538dbd
Gleefre <varedif.a.s@gmail.com>
2022-11-15 13:15:51 +0100

    fix font bug

--- app.lisp
+++ app.lisp
@@ -8,6 +8,20 @@
   (incf i)
   (background (hash-color (floor i 10))))
 
+(defparameter *font-name* "RobotoMono-Bold.ttf")
+
+(defun sketch::make-default-font ())
+(defun sketch::make-error-font ())
+
+(defun start ()
+  #+deploy
+  (sdl2:make-this-thread-main
+   (lambda ()
+     (let ((sketch::*build* t))
+       (make-instance 'app :resizable t))))
+  #-deploy
+  (make-instance 'app :resizable t))
+
 (defun start ()
   (make-instance 'app)
   (sleep 5))
